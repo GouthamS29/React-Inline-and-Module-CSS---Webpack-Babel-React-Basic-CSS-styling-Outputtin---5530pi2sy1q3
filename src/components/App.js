62e3234a82fe9175a6a5fc3ebc07cea8bffce346
GouthamS29 <sambarajugoutham1994@gmail.com>
2024-04-12 00:23:46 +0530

    Styling file name update

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/App.css';
-import styles from '../styles/appStyles.module.css';
+import styles from '../styles/appStyle.module.css';
 
 const App = () => {
 const heading = {fontSize: "50px", color: "red"};
