d80c0dbfe2bce8edb11d4a2ee4548f72d399f376
GouthamS29 <sambarajugoutham1994@gmail.com>
2024-04-12 00:22:17 +0530

    plugin updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles/App.css';
 import styles from '../styles/appStyles.module.css';
+
 const App = () => {
 const heading = {fontSize: "50px", color: "red"};
   return (
